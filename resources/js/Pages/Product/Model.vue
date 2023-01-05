b1c2aecb1dee5ad8a058827854332e72335a73d1
sibber11 <sibber11@gmail.com>
2023-01-05 21:47:30 +0600

    reformat and key added

--- resources/js/Pages/Product/Model.vue
+++ resources/js/Pages/Product/Model.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {Link} from "@inertiajs/inertia-vue3";
+
 const props = defineProps({
     product: Object
 })
@@ -8,25 +9,26 @@ const props = defineProps({
 <template>
     <div class="p-2 sm:p-4 bg-white shadow sm:rounded-lg">
         <div class="">
-            <h3 class="text-xl font-bold text-gray-800 capitalize">{{product.name}}</h3>
+            <h3 class="text-xl font-bold text-gray-800 capitalize">{{ product.name }}</h3>
         </div>
         <div class="pt-2">
-            <div class="text-sm text-gray-600">SKU: {{product.sku}}</div>
-            <div class="text-sm text-gray-600">Category: {{product.category.name}}</div>
-            <div class="text-sm text-gray-600">Price: {{product.price}}</div>
-<!--            <div class="text-sm text-gray-600">Brand: {{product.brand}}</div>-->
-            <p class="text-gray-800">Description: {{product.description}}</p>
+            <div class="text-sm text-gray-600">SKU: {{ product.sku }}</div>
+            <div class="text-sm text-gray-600" v-if="product.category">Category: {{ product.category.name }}</div>
+            <div class="text-sm text-gray-600">Price: {{ product.price }}</div>
+            <!--            <div class="text-sm text-gray-600">Brand: {{product.brand}}</div>-->
+            <p class="text-gray-800">Description: {{ product.description }}</p>
             <div class="text-sm text-gray-600">
                 <span>Tags: </span>
                 <template v-if="product.tags.length > 0">
-                    <span v-for="tag in product.tags" :key="tag.id" class="text-cyan-500"> {{tag.name.en}},  </span>
+                    <span v-for="tag in product.tags" :key="tag.id" class="text-cyan-500"> {{ tag.name.en }},  </span>
                 </template>
                 <span v-else> No Tags </span>
             </div>
             <Link :href="route('products.edit',product)" class="text-blue-500 hover:underline">
                 Edit
             </Link>
-            <Link :href="route('products.destroy',product)" as="button" method="delete" class="text-red-600 hover:underline">
+            <Link :href="route('products.destroy',product)" as="button" method="delete"
+                  class="text-red-600 hover:underline">
                 Delete
             </Link>
         </div>
