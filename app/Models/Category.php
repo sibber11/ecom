e8c0681c2251eabe57f410094a0f2231d09e045a
sibber11 <sibber11@gmail.com>
2023-01-11 19:37:25 +0600

    slug added and parent name appended

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,17 +5,40 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Kalnoy\Nestedset\NodeTrait;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Category extends Model
 {
-    use HasFactory, NodeTrait;
+    use HasFactory, NodeTrait, HasSlug;
+
     public $timestamps = false;
     protected $fillable = [
         'name'
+    ];
+    protected $appends = [
+        'parent_name'
     ];
 
     public function products()
     {
         return $this->hasMany(Product::class);
+    }
+
+    public function getParentNameAttribute()
+    {
+        return $this->parent?->name;
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
     }
 }
