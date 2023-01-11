f1b634603801e35bd8cd0c023940dd135dc501d9
sibber11 <sibber11@gmail.com>
2023-01-11 19:24:21 +0600

    inertiajs datatable added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,11 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination.vue/resources/views/*.blade.php',
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
+        './node_modules/@protonemedia/inertiajs-tables-laravel-query-builder/**/*.{js,vue}',
     ],
 
     theme: {
