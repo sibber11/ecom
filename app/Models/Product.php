76882f2bed1e2c7525ba61ec5fe4ec09a4d4baf6
sibber11 <sibber11@gmail.com>
2023-01-11 19:38:02 +0600

    quantity, media and brand added

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,19 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 use Spatie\Tags\HasTags;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
-    use HasFactory, HasSlug, HasTags;
+    use HasFactory, HasSlug, HasTags, InteractsWithMedia;
+
+    protected $with = ['brand', 'category'];
+
     protected $fillable = [
         'name',
         'sku',
         'description',
         'category_id',
         'price',
+        'quantity',
+        'brand_id',
     ];
 
     public function getSlugOptions(): SlugOptions
@@ -32,5 +39,10 @@ class Product extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
     }
 }
