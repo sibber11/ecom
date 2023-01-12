d91d3473b4b66d205ff603ce2c156d73889a4478
sibber11 <sibber11@gmail.com>
2023-01-12 18:38:44 +0600

    Edit Delete Button added

---
+++ resources/js/components/DeleteButton.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import {InertiaLink} from "@inertiajs/inertia-vue3";
+
+defineProps({
+    url: String
+})
+
+</script>
+
+<template>
+    <InertiaLink :href="url" as="button" method="delete" class="px-2 py-1 border mx-1 rounded bg-red-600 text-white">
+        Delete
+    </InertiaLink>
+</template>
---
+++ resources/js/components/EditButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+import {InertiaLink} from "@inertiajs/inertia-vue3";
+
+const props = defineProps({
+    url: String
+})
+</script>
+<template>
+    <InertiaLink :href="url" class="px-2 py-1 border mx-1 rounded">
+        Edit
+    </InertiaLink>
+</template>
