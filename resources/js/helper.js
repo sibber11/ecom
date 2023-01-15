91c4ab63580cd906f5835d99b6e736ab882996ca
sibber11 <sibber11@gmail.com>
2023-01-15 18:47:24 +0600

    patch method fixed on helper.js

--- resources/js/helper.js
+++ resources/js/helper.js
@@ -3,6 +3,7 @@ export function submitForm(_method, _form, _url){
         _form.post(_url)
     }
     if (_method === 'patch'){
-        _form.patch(_url)
+        _form._method = 'patch'
+        _form.post(_url)
     }
 }
