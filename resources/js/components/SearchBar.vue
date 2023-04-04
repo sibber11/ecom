388951cb0746c741aa21ec2f2ec559c4b3034c68
sibber11 <sibber11@gmail.com>
2023-04-04 20:11:19 +0600

    search result fallback image added

--- resources/js/components/SearchBar.vue
+++ resources/js/components/SearchBar.vue
@@ -7,7 +7,7 @@ const products = ref([]);
 
 //use debounce for the getProducts function by 300ms
 const getProducts = debounce(() => {
-    if (query.value.length < 3) return
+    if (query.value.length < 1) return
     axios.get(route('search'), {
         params: {
             query: query.value
@@ -33,21 +33,23 @@ const focused = ref(true);
         <div class="absolute w-full left-0 z-10 px-4">
             <ul class="bg-white border shadow-2xl rounded-b-md w-full p-1 ">
                 <li v-for="product in products" :key="product.id" class="hover:bg-red-200 bg-white p-1 m-1 border">
-                    <a :href="route('products.show', product)" class="flex justify-between items-center">
-                    <span class="inline-flex items-center">
-                        <img :src="product.image" alt="product image" class="w-12 h-12">
-                        <span class="ml-2 text-sm text-gray-600">{{ product.name }}</span>
-                    </span>
-                        <div>
-                        <span class="ml-2 text-sm text-gray-600">
-                            {{ product.reviews_avg_rating }}
-                            <i class="fa fa-star text-primary"></i>
-                        </span>
-                            <span class="text-sm text-gray-600">
-                            ({{ product.reviews_count }})
-                        </span>
+                    <a :href="route('products.show', product)" class="grid grid-cols-3 items-center">
+                        <div class="inline-flex items-center">
+                            <img :src="product.image" alt="product image" class="w-12 h-12"
+                                 onerror="this.src='https://via.placeholder.com/48'"
+                            >
+                            <span class="ml-2 text-sm text-gray-600">{{ product.name }}</span>
                         </div>
-                        <span class="text-sm text-gray-600">${{ product.price }}</span>
+                        <div class="text-right">
+                            <span class="ml-2 text-sm text-gray-600">
+                                {{ product.reviews_avg_rating }}
+                                <i class="fa fa-star text-primary"></i>
+                            </span>
+                                <span class="text-sm text-gray-600">
+                                ({{ product.reviews_count }})
+                            </span>
+                        </div>
+                        <div class="text-sm text-gray-600 text-right">${{ product.price }}</div>
                     </a>
                 </li>
             </ul>
