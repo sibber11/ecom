149980e3f2fe54f56d88af608b420d8c1a6429ec
sibber11 <sibber11@gmail.com>
2023-05-24 13:08:50 +0600

    mark all notification as read

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\BrandController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\NotificationController;
+use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\ProductAttributeController;
 use Inertia\Inertia;
 
 Route::middleware(['auth','admin'])->group(function (){
@@ -11,31 +18,34 @@ Route::middleware(['auth','admin'])->group(function (){
         return Inertia::render('Dashboard');
     });
 
-    Route::resource('products', \App\Http\Controllers\Admin\ProductController::class);
-    Route::post('products/get_slug', [\App\Http\Controllers\Admin\ProductController::class, 'get_slug'])
+    Route::resource('products', ProductController::class);
+    Route::post('products/get_slug', [ProductController::class, 'get_slug'])
         ->name('products.get_slug');
-    Route::delete('products/{product}/{media}', [\App\Http\Controllers\Admin\ProductController::class, 'deleteMedia'])->name('products.deleteMedia');
+    Route::delete('products/{product}/{media}', [ProductController::class, 'deleteMedia'])->name('products.deleteMedia');
 
-    Route::resource('categories', \App\Http\Controllers\Admin\CategoryController::class)
+    Route::resource('categories', CategoryController::class)
         ->except('show');
-    Route::post('categories/get_names', [\App\Http\Controllers\Admin\CategoryController::class, 'get_names'])
+    Route::post('categories/get_names', [CategoryController::class, 'get_names'])
         ->name('categories.get_names');
-    Route::post('categories/get_slug', [\App\Http\Controllers\Admin\CategoryController::class, 'get_slug'])
+    Route::post('categories/get_slug', [CategoryController::class, 'get_slug'])
         ->name('categories.get_slug');
-    Route::delete('categories/{category}/{media}', [\App\Http\Controllers\Admin\CategoryController::class, 'deleteMedia'])
+    Route::delete('categories/{category}/{media}', [CategoryController::class, 'deleteMedia'])
         ->name('categories.deleteMedia');
 
-    Route::resource('brands', \App\Http\Controllers\Admin\BrandController::class);
-    Route::post('brands/get_names', [\App\Http\Controllers\Admin\BrandController::class, 'get_names'])
+    Route::resource('brands', BrandController::class);
+    Route::post('brands/get_names', [BrandController::class, 'get_names'])
         ->name('brands.get_names');
-    Route::post('brands/get_slug', [\App\Http\Controllers\Admin\BrandController::class, 'get_slug'])
+    Route::post('brands/get_slug', [BrandController::class, 'get_slug'])
         ->name('brands.get_slug');
 
-    Route::resource('users', \App\Http\Controllers\Admin\UserController::class);
+    Route::resource('users', UserController::class);
 
-    Route::resource('orders', \App\Http\Controllers\Admin\OrderController::class);
+    Route::resource('orders', OrderController::class);
 
-    Route::resource('attributes', \App\Http\Controllers\ProductAttributeController::class);
+    Route::resource('attributes', ProductAttributeController::class);
+
+    Route::post('read-notifications', [NotificationController::class, 'readNotifications'])
+        ->name('read-notifications');
 });
 
 require __DIR__ . '/admin_auth.php';
--- routes/channels.php
+++ routes/channels.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('order-placed', function ($user) {
     return true;
 });
+
+Broadcast::channel('admin-notifications', function ($user) {
+    return $user->isAdmin();
+});
