19c0d1d10ce9bf30b3f86e6fcb4f153a2c04e6f2
sibber11 <sibber11@gmail.com>
2023-05-24 16:21:55 +0600

    file renamed

--- app/Notifications/NewOrderNotification.php
+++ app/Notifications/NewOrderNotification.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Messages\BroadcastMessage;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class NotifyAdminNotification extends Notification implements ShouldQueue
+class NewOrderNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
